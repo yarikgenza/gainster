9d6f06e539b8d3472f305159d62af799549b6546
Yarik Genza <yaroslav.genza@vedidev.com>
2017-10-21 19:42:34 +0300

    animate logo

--- src/screens/LaunchScreen.js
+++ src/screens/LaunchScreen.js
@@ -1,35 +1,31 @@
+import * as Animatable from 'react-native-animatable';
 import React, { Component } from 'react';
 import { StyleSheet, Image } from 'react-native';
 import { Container, Content, H1, Body, Text, Icon } from 'native-base';
 import theme from '../themes';
 
-export default class LaunchScreen extends Component {
+class LaunchScreen extends Component {
   render = () => (
     <Container>
       <Content>
-        <Image
+        <Animatable.Image
+          animation="fadeIn"
           style={styles.background}
           source={require('../images/splash.png')}
         >
-          <Image
+          <Animatable.Image
+            animation="flipInX"
+            delay={300}
             source={require('../images/logo.png')}
             style={styles.logo}
           />
-          <Text style={styles.h1}>Gainster</Text>
-          <Icon name="ios-arrow-forward" style={{fontSize: 50, margin: 20, marginLeft: (theme.deviceWidth / 2) - 10, color: 'white'}} />
-        </Image>
+        </Animatable.Image>
       </Content>
     </Container>
   );
 }
 
 const styles = StyleSheet.create({
-  h1: {
-    color: theme.appNameTextColor,
-    fontSize: 50,
-    textAlign: 'center',
-    marginTop: theme.deviceHeight / 5
-  },
   background: {
     flex: 1,
     alignSelf: 'stretch',
@@ -42,4 +38,12 @@ const styles = StyleSheet.create({
     marginTop: theme.deviceHeight / 10,
     alignSelf: 'center',
   }
+  /* h1: {
+    color: theme.appNameTextColor,
+    fontSize: 50,
+    textAlign: 'center',
+    marginTop: theme.deviceHeight / 5
+  }, */
 });
+
+export default Animatable.createAnimatableComponent(LaunchScreen);
