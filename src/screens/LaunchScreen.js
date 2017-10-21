6b59e0be3014b69c87883fb25670b1e72d584ba0
Yarik Genza <yaroslav.genza@vedidev.com>
2017-10-21 23:49:21 +0300

    add auth status handling

--- src/screens/LaunchScreen.js
+++ src/screens/LaunchScreen.js
@@ -1,31 +1,62 @@
 import * as Animatable from 'react-native-animatable';
 import React, { Component } from 'react';
 import { StyleSheet, Image } from 'react-native';
-import { Container, Content, H1, Body, Text, Icon } from 'native-base';
+import { observable } from 'mobx';
+import { observer } from 'mobx-react';
+import { Container, Content, H1, Body, Text, Icon, Spinner } from 'native-base';
 import theme from '../themes';
 
 import FacebookLogin from '../components/FacebookLogin';
+import isUserAuthorized from '../utils/checkFbAuth';
 
+@observer
 class LaunchScreen extends Component {
-  render = () => (
-    <Container>
-      <Content>
-        <Animatable.Image
-          animation="fadeIn"
-          style={styles.background}
-          source={require('../images/splash.png')}
-        >
+
+  @observable isLoading = true;
+  @observable isAuthorized = null;
+
+  async componentDidMount() {
+    try {
+      const isAuthorized = await isUserAuthorized();
+      if (isAuthorized) {
+        this.isAuthorized = true;
+        this.isLoading = false;
+      } else {
+        this.isAuthorized = false;
+        this.isLoading = false;
+      }
+    } catch (e) {
+      console.error(e);
+    }
+  }
+
+  render() {
+    const { isLoading, isAuthorized } = this;
+
+    const renderContent = () => isAuthorized ? (
+      <Text>Hello, user!</Text> 
+    ) : (<FacebookLogin />)
+
+    return (
+      <Container>
+        <Content>
           <Animatable.Image
-            animation="flipInX"
-            delay={300}
-            source={require('../images/logo.png')}
-            style={styles.logo}
-          />
-          <FacebookLogin />
-        </Animatable.Image>
-      </Content>
-    </Container>
-  );
+            animation="fadeIn"
+            style={styles.background}
+            source={require('../images/splash.png')}
+          >
+            <Animatable.Image
+              animation="flipInX"
+              delay={300}
+              source={require('../images/logo.png')}
+              style={styles.logo}
+            />
+            {isLoading ? (<Spinner color="white" />) : renderContent()}
+          </Animatable.Image>
+        </Content>
+      </Container>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
